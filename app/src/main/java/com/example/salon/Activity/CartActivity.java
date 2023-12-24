8db396b7397a4875cdf132c8511c49f2384feb20
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-12-24 23:27:05 +0700

    add non-visible to place button

--- app/src/main/java/com/example/salon/Activity/CartActivity.java
+++ app/src/main/java/com/example/salon/Activity/CartActivity.java
@@ -1,11 +1,15 @@
 package com.example.salon.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.example.salon.Adapter.CartAdapter;
 import com.example.salon.Helper.ChangeNumberItemsListener;
@@ -14,7 +18,6 @@ import com.example.salon.R;
 import com.example.salon.databinding.ActivityCartBinding;
 
 public class CartActivity extends BaseActivity {
-    private ActivityCartBinding binding;
     private RecyclerView.Adapter adapter;
     private ManagmentCart managmentCart;
     private double tax;
@@ -22,8 +25,7 @@ public class CartActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityCartBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_cart);
 
         managmentCart = new ManagmentCart(this);
 
@@ -33,18 +35,22 @@ public class CartActivity extends BaseActivity {
     }
 
     private void initList() {
+        TextView emptyTxt = findViewById(R.id.emptyTxt);
+        ScrollView scrollviewCart = findViewById(R.id.scrollviewCart);
+        RecyclerView cardView = findViewById(R.id.cardView);
+
         if (managmentCart.getListCart().isEmpty()) {
-            binding.emptyTxt.setVisibility(View.VISIBLE);
-            binding.scrollviewCart.setVisibility(View.GONE);
+            emptyTxt.setVisibility(View.VISIBLE);
+            scrollviewCart.setVisibility(View.GONE);
         } else {
-            binding.emptyTxt.setVisibility(View.GONE);
-            binding.scrollviewCart.setVisibility(View.VISIBLE);
+            emptyTxt.setVisibility(View.GONE);
+            scrollviewCart.setVisibility(View.VISIBLE);
         }
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        binding.cardView.setLayoutManager(linearLayoutManager);
+        cardView.setLayoutManager(linearLayoutManager);
         adapter = new CartAdapter(managmentCart.getListCart(), this, () -> calculateCart());
-        binding.cardView.setAdapter(adapter);
+        cardView.setAdapter(adapter);
     }
 
     private void calculateCart() {
@@ -56,13 +62,32 @@ public class CartActivity extends BaseActivity {
         double total = Math.round((managmentCart.getTotalFee() + tax + delivery) * 100) / 100;
         double itemTotal = Math.round(managmentCart.getTotalFee() * 100) / 100;
 
-        binding.totalFeeTxt.setText("$" + itemTotal);
-        binding.taxTxt.setText("$" + tax);
-        binding.deliveryTxt.setText("$" + delivery);
-        binding.totalTxt.setText("$" + total);
+
+        TextView totalFeeTxt = findViewById(R.id.totalFeeTxt);
+        TextView taxTxt = findViewById(R.id.taxTxt);
+        TextView deliveryTxt = findViewById(R.id.deliveryTxt);
+        TextView totalTxt = findViewById(R.id.totalTxt);
+
+        AppCompatButton orderBtn = findViewById(R.id.orderBtn);
+
+        totalFeeTxt.setText("$" + itemTotal);
+        taxTxt.setText("$" + tax);
+        deliveryTxt.setText("$" + delivery);
+        totalTxt.setText("$" + total);
+
+        if(itemTotal>0)
+        {
+            orderBtn.setVisibility(View.VISIBLE);
+        }
+        else {
+            orderBtn.setVisibility(View.GONE);
+        }
+
     }
 
     private void setVariable() {
-        binding.backBtn.setOnClickListener(v -> finish());
+
+        ImageView backBtn = findViewById(R.id.backBtn);
+       backBtn.setOnClickListener(v -> finish());
     }
 }
